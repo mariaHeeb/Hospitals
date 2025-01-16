781cf8d7af5b6f71ec777ccf92122148c1b78b62
MariaHeeb <mariahip3@gmail.com>
2025-01-16 22:10:47 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/src/main/res/layout/activity_sign_up.xml

--- app/src/main/java/com/example/hospitals/LogInActivity.java
+++ app/src/main/java/com/example/hospitals/LogInActivity.java
@@ -15,10 +15,6 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
